9345a91b6413742b576de0c59feed3d56ead0489
OdairMarra <marra.odair@gmail.com>
2023-09-30 16:33:41 -0300

    Update - open web

--- src/2gather/App.js
+++ src/2gather/App.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    backgroundColor: 'magenta',
     padding: 8,
   },
   paragraph: {
