4af0254398bc88be0db655ded98574227b10a358
OdairMarra <marra.odair@gmail.com>
2023-10-03 14:55:00 -0300

    feature-172
    Start building the Screens: Home, Login

--- src/2gather/App.js
+++ src/2gather/App.js
@@ -1,35 +1,30 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import React from 'react';
+/*import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Keyboard,
+  KeyboardAvoidingView,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Animated
+} from 'react-native';
 
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
+import { Button } from 'react-native-paper';*/
 
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
+import Home from './screens/Home';
+import Login from './screens/Login';
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </SafeAreaView>
-  );
+
+
+const App = () => {
+return (
+  <Login />
+)
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'magenta',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+export default App;
---
+++ src/2gather/screens/Home.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
+
+const Home = () => {
+  return (
+    <View style={styles.body}>
+      <Text style={styles.textGoodSeeYou}>
+        Olá, bom te ver!
+      </Text>
+
+      <View>
+        <Image
+          style={{
+            width: 135,
+            height: 155,
+            borderRadius: 15,
+          }}
+          source={require('../assets/logo.png')}
+        />
+      </View>
+
+      <View style={styles.buttonLogin}>
+        <Button icon="login" mode="contained" onPress={() => console.log('Pressed')}>
+          Fazer login
+        </Button>
+      </View>
+    </View>
+  )
+};
+
+
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#2368A2'   
+  },
+
+  textGoodSeeYou: {
+    margin: 50,
+    marginBottom: 150,
+    fontSize: 30,
+    color: '#FFFFFF'
+  },
+
+  buttonLogin: {
+    marginTop: 150,
+    width: '75%',
+    buttonColor: '#1A4971',
+    textColor: '#FFFFFF',
+    marginBottom: 15,
+    fontSize: 17,
+    borderRadius: 10,
+  }
+
+});
+
+export default Home;
