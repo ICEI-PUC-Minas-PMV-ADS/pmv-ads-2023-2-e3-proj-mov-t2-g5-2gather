062da6d0c50c4c50ba81f565acecb22e3340850c
OdairMarra <marra.odair@gmail.com>
2023-10-06 04:57:33 -0300

    feature-179 [tests]

---
+++ src/2gather/navigations/Route.js
@@ -0,0 +1,28 @@
+/*import React from 'react';
+
+import { useUser } from '../contexts/UserContext';
+
+import Main from './Main';
+//import Auth from './Auth';
+
+
+const Main = () => {
+
+    const { signed } = useUser();
+
+        return (
+            <>
+            {
+                signed
+                ? <Main />
+                : <Auth />
+            }
+            </>
+        )
+
+
+
+
+}
+
+export default Main;*/
