4492f4f096ac5cc85cde8a02dfb25aafb2004dfc
leocssilva <leocssilva@gmail.com>
2023-10-18 21:16:02 -0300

    [feature] update createuser edituser user.services

--- src/2gather/services/user.services.js
+++ src/2gather/services/user.services.js
@@ -1,5 +1,7 @@
 import { sendAuthenticatedRequest } from './auth.services.js'
 
+const API_URL = process.env.NODE_ENV === 'development' ? REACT_APP_DEV_MODE : REACT_APP_PROD_MODE;
+
 export const GetUserList = async () => {
     try {
         const result = await sendAuthenticatedRequest('/user/list/', 'GET');
@@ -13,6 +15,23 @@ export const UpdateUserStatus = async ({userId, reason}) => {
     try {
         const data = { 'status':0 };
         const result = await sendAuthenticatedRequest(`/user/update/${userId}/admin/`, 'PATCH', data);
+        return result;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+export const UpdateUserDetails = async ({ name, email, phone, photo, description, idRole, lastActive, status }) => {
+    try {
+        const response = await fetch(`${API_URL}user/update/${uuid}/admin/`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ name, email, phone, photo, description, idRole, lastActive, status })
+        });
+        const result = await response.json();
+        if (!response.ok) {
+            throw new Error(JSON.stringify(result));
+        }
         return result;
     } catch (error) {
         throw new Error(error.message);
